c3afcadbfa9fe8ab013bcc4f751ae6f365b180b2
Nishant Bhadani <itisnishant@gmail.com>
2019-07-27 16:54:15 +1000

    common

---
+++ src/main/java/common/MoveZerosToEnd.java
@@ -0,0 +1,23 @@
+package common;
+
+
+/** Given an array containing zeros,
+ * write a function which will move all zeros to the end of array */
+public class MoveZerosToEnd {
+    public void move(int[] arr) {
+
+        int count = 0;
+        for(int i = 0 ; i < arr.length ; i++) {
+            if(arr[i]!=0){
+                arr[count++] = arr[i];
+            }
+        }
+
+        //copy the zeros
+
+        for(int j = count ; j < arr.length ; j++) {
+            arr[j] = 0;
+        }
+
+    }
+}
