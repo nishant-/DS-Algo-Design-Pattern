8a01784e1d418cea4217ff0cbb63dece91b1d647
Nishant Bhadani <itisnishant@gmail.com>
2020-01-27 16:21:05 +1100

    binary tree

--- src/main/java/ds/tree/BinaryTree.java
+++ src/main/java/ds/tree/BinaryTree.java
@@ -1,15 +1,94 @@
 package ds.tree;
 
-import java.util.Comparator;
-
 public class BinaryTree<T extends Comparable<T>> {
 
+    private BTNode<T> rootNode;
 
-
-
-    private static class Node<T> {
-        Node<T> leftChild;
-        Node<T> rightChild;
-        T data;
+    public BTNode getRootNode() {
+        return rootNode;
     }
+
+    public void setRootNode(BTNode rootNode) {
+        this.rootNode = rootNode;
+    }
+
+    public void insert(T key) {
+
+        BTNode tNode = new BTNode(key);
+        insert(this, tNode);
+    }
+
+    private void insert(BinaryTree<T> binaryTree, BTNode tNode) {
+
+        //get the root node
+        BTNode<T> rootNode = binaryTree.getRootNode();
+        if(rootNode == null) {
+            // tree is empty
+            binaryTree.setRootNode(tNode);
+            return;
+        }
+        else {
+            // insert the key at correct position
+            BTNode<T> tmp = rootNode;
+            BTNode<T> parent;
+            while(true) {
+                parent = tmp;
+                if(tNode.data.compareTo(parent.data) > 0) {
+                    //go right
+                    tmp = tmp.rightChild;
+                    if (tmp == null) {
+                        parent.rightChild = tNode;
+                        return;
+                    }
+                }
+                    else { // go left
+                        tmp = tmp.leftChild;
+                        if(tmp == null) {
+                            parent.leftChild = tNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+
+     static class BTNode<T extends Comparable<T>> {
+        BTNode<T> leftChild;
+        BTNode<T> rightChild;
+        T data;
+
+        public BTNode(T data) {
+            this.data = data;
+            leftChild = null;
+            rightChild = null;
+        }
+    }
+
+    public void inOrderTraversal() {
+        inOrderTraversal(this.getRootNode());
+    }
+
+    private void inOrderTraversal(BTNode<T> node) {
+
+        if(node == null)
+            return;
+        inOrderTraversal(node.leftChild);
+        System.out.println(node.data);
+        inOrderTraversal(node.rightChild);
+
+    }
+
+    //returns total number of nodes in a tree
+    public int size() {
+        return size(this.getRootNode());
+    }
+
+    private int size(BTNode<T> node) {
+
+        return (node == null) ? 0 : (size(node.rightChild) + size(node.leftChild) + 1);
+    }
+
+
+
 }
---
+++ src/main/java/ds/tree/TestBtree.java
@@ -0,0 +1,21 @@
+package ds.tree;
+
+public class TestBtree {
+
+    public static void main(String[] args) {
+
+        BinaryTree<Integer> btree = new BinaryTree<>();
+        btree.insert(5);
+        btree.insert(6);
+        btree.insert(3);
+        btree.insert(4);
+        btree.insert(2);
+
+
+        btree.inOrderTraversal();
+        System.out.println(btree.size());
+
+
+
+    }
+}
