8c0ab016adee3f49a4286401d6acab2bd6661a53
Nishant Bhadani <itisnishant@gmail.com>
2020-02-15 08:22:07 +1100

    max perimeter triangle fixed

--- src/main/java/hackerrank/problem/solving/MaxPerimeterTriangle.java
+++ src/main/java/hackerrank/problem/solving/MaxPerimeterTriangle.java
@@ -17,35 +17,13 @@ public class MaxPerimeterTriangle {
     // Complete the maximumPerimeterTriangle function below.
     public static int[] maximumPerimeterTriangle(int[] sticks) {
 
-        long maxPerimeter = -1;
-        int arr[] = new int[3];
-        boolean triangleExists = false;
+        Arrays.sort(sticks);
 
-       for(int i = sticks.length - 1; i >= 1; i--) {
-
-            int a = sticks[i];
-            int b = sticks[i - 1];
-
-            for(int j = i+2; j < sticks.length; j++) {
-                int c = sticks[j];
-
-                if(isValidTriangle(a, b, c)) {
-                    long perimeter = a + b + c;
-                    if(perimeter > maxPerimeter) {
-                        maxPerimeter = perimeter;
-                        arr[0] = a;
-                        arr[1] = b;
-                        arr[2] = c;
-                        triangleExists = true;
-                    }
-                }
-            }
+        int i = sticks.length - 3;
+        while(i >= 0 && (sticks[i] + sticks[i + 1]) <= sticks[i + 2]) {
+            i--;
         }
-        return triangleExists ? arr : new int[]{-1};
-    }
-
-    private static boolean isValidTriangle(int a, int b, int c) {
-        return ((a + b > c) && (a + c > b) && (b + c > a));
+        return (i >=0) ? new int[]{sticks[i], sticks[i + 1], sticks[i + 2]} : new int []{-1};
     }
 
     private static final Scanner scanner = new Scanner(System.in);
